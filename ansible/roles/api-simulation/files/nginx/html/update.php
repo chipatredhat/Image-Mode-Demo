389a5608fc34c08de54dda6e822ce971dc4d0051
Chip Shabazian <chip@shabazian.com>
2025-09-03 15:45:51 -0700

    Change path to cgi-bin

--- ansible/roles/api-simulation/files/nginx/html/update.php
+++ ansible/roles/api-simulation/files/nginx/html/update.php
@@ -1,5 +1,5 @@
 <?php
-shell_exec("/usr/share/nginx/cgi-bin/update.sh")
+shell_exec("/var/www/cgi-bin/update.sh")
 ?>
 <center><h1 style="color: red;">Updating and rebooting <u>petclinic.example.com</u></h1></center>
 <meta http-equiv="refresh" content="3;url=http://server.example.com/useapi.php" />
