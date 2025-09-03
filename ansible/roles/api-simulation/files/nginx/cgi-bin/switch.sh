4da53893d522292545c868309358b1f94fee4e39
Chip Shabazian <chip@shabazian.com>
2025-09-03 13:51:37 -0700

    add api alternatives

--- ansible/roles/api-simulation/files/nginx/cgi-bin/switch.sh
+++ ansible/roles/api-simulation/files/nginx/cgi-bin/switch.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-curl -s -k -c - -X POST --user 'admin:redhat' https://aap.acme.com/api/controller/v2/job_templates/17/launch/
+
+# The following command uses the Ansible AAP API to run a job template
+#curl -s -k -c - -X POST --user 'admin:redhat' https://aap.acme.com/api/controller/v2/job_templates/17/launch/
+
+# This command runs a local playbook when AAP isn't available
+ansible-playbook /home/lab-user/ansible/switch.yaml
