d9cb45f027dbbde1d3b89456267cf14b2cbef99e
chipatredhat <cshabazi@redhat.com>
2025-10-08 15:39:38 -0700

    Move password change to display-results in case it needs to relaunch

--- deploy.sh
+++ deploy.sh
@@ -30,7 +30,7 @@ REGISTRY_ACCOUNT=$(grep PMUSERNAME ${CREDENTIALS_DIRECTORY}/credentials.yml | aw
 }
 
 function CHECK_API_TOKEN() {
-if [ ! -f ${API_TOKEN} ] ; then
+if [ "${API_TOKEN}" = "null" ] ; then
     echo -e "\n\nYour API Token isn't stored in ${CREDENTIALS_DIRECTORY}/credentials.yml.  If you do not currently have one, you can create one at https://access.redhat.com/management/api"
     read -p "What is your API Token? " API_TOKEN
     echo -e "\n"
@@ -41,6 +41,50 @@ if [ ! -f ${API_TOKEN} ] ; then
 fi
 }
 
+function CHECK_REGISTRY_TOKEN() {
+if [ "${REGISTRY_TOKEN}" = "null" ] ; then
+    echo -e "\n\nYour Registry Token isn't stored in ${CREDENTIALS_DIRECTORY}/credentials.yml.   If you do not currently have one, you can create one at https://access.redhat.com/terms-based-registry"
+    read -p "What is your Registry Token? " REGISTRY_TOKEN
+    echo -e "\n"
+    read -n1 -p "Would you like to save your  Registry Token to ${CREDENTIALS_DIRECTORY}/credentials.yml to allow deployments to automatically build? (Y/N) " SAVE_REGISTRY_TOKEN
+    if [ "${SAVE_REGISTRY_TOKEN}" = "Y" ] || [ "${SAVE_REGISTRY_TOKEN}" = "y" ] ; then
+        sed -i '' -e "s|PMPASSWORD: .*|PMPASSWORD: ${REGISTRY_TOKEN}|g" ${CREDENTIALS_DIRECTORY}/credentials.yml
+    fi
+fi
+}
+
+function CHECK_REGISTRY_ACCOUNT() {
+if [ "${REGISTRY_ACCOUNT}" = "null" ] ; then
+    echo -e "\n\nYour Registry Account Name isn't stored in ${CREDENTIALS_DIRECTORY}/credentials.yml.   If you do not currently have one, you can create one at https://access.redhat.com/terms-based-registry"
+    read -p "What is your Registry Account Name? " REGISTRY_ACCOUNT
+    echo -e "\n"
+    read -n1 -p "Would you like to save your Registry Account Name to ${CREDENTIALS_DIRECTORY}/credentials.yml to allow deployments to automatically build? (Y/N) " SAVE_REGISTRY_ACCOUNT
+    if [ "${SAVE_REGISTRY_ACCOUNT}" = "Y" ] || [ "${SAVE_REGISTRY_ACCOUNT}" = "y" ] ; then
+        sed -i '' -e "s|PMUSERNAME: .*|PMUSERNAME: ${REGISTRY_ACCOUNT}|g" ${CREDENTIALS_DIRECTORY}/credentials.yml
+    fi
+fi
+}
+
+function VERIFY_API_TOKEN() {
+token=$(curl -s https://sso.redhat.com/auth/realms/redhat-external/protocol/openid-connect/token -d grant_type=refresh_token -d client_id=rhsm-api -d refresh_token=$API_TOKEN | jq --raw-output .access_token)
+if [ "${token}" = "null" ] ; then
+    echo "Your API Token is not valid.  Please create an updated token at https://access.redhat.com/management/api"
+    read -p "Press any key to restart this script and enter a new API Token or Ctrl-C to escape" -n1 -s
+    exec bash "$0" "$@" # Have script restart after updating
+fi
+}
+
+
 UPDATE
 GET_CREDENTIALS
 CHECK_API_TOKEN
+CHECK_REGISTRY_TOKEN
+CHECK_REGISTRY_ACCOUNT
+VERIFY_API_TOKEN
+
+# Ask if this should deploy the full pipeline:
+read -n1 -p "Do you wish to install the full pipeline? (Y/N) " INSTALL_PIPELINE
+echo -e "\n"
+
+read -n1 -p "Are you deploying into a CNV Baremetal instance from demo.redhat.com?" RHDP
+echo -e "\n"
