b00346db163c9c25b8abb1fb2fb2e63edbde5dce
chipatredhat <cshabazi@redhat.com>
2026-04-21 08:58:06 -0700

    Move local copy to functions

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # Use a local credentials file if it exists
-if [ -f "~/.secrets/credentials.yml" ] ; then
+if [ -f "${HOME}/.secrets/credentials.yml" ] ; then
     CREDENTIALS_FILE=${HOME}/.secrets/credentials.yml
 else
     CREDENTIALS_FILE=${SCRIPT_DIR}/ansible/credentials.yml
@@ -31,5 +31,5 @@ CHECK_REGISTRY_ACCOUNT
 VERIFY_API_TOKEN
 GET_BUILD_PARAMETERS
 WRITE_FILES
-if [[ "${LOCAL_COPY}" == "1" && -n "${LOCAL_SOURCE}" ]]; then COPY_ISO_FILES ; fi # See functions for description
+COPY_ISO_FILES # See functions for description
 INSTALL_DEMO
