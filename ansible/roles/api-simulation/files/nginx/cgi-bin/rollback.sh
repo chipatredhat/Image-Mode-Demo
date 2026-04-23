44205488e6cebb0e91b032d471388bcff7957a5e
chipatredhat <cshabazi@redhat.com>
2026-04-23 09:37:21 -0700

    Update deployment scripts

--- ansible/roles/api-simulation/files/nginx/cgi-bin/rollback.sh
+++ ansible/roles/api-simulation/files/nginx/cgi-bin/rollback.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "Content-type: text/html"
 echo ""
-echo "<html><head><meta http-equiv='refresh' content='3;url=/useapi.php'></head>"
+echo "<html><head><meta http-equiv='refresh' content='5;url=/useapi.php'></head>"
 echo "<body style='font-family:sans-serif; padding:20px;'>"
 echo "<center>"
-echo "<h1>Running rollback on Petclinic Application Server</h1><hr><pre></center>"
-sudo -u lab-user ansible-playbook -v /home/lab-user/ansible/rollback.yaml -i appserver, 2>&1
+echo "<h1>Running rollback on Petclinic Application Server and rebooting into previous image</h1><hr><pre></center>"
+sudo -i -u lab-user tmux new-session -d -s rollback 'ansible-playbook -vvv /home/lab-user/ansible/rollback.yaml -i appserver, 2>&1 > /tmp/rollback_output.txt'
 echo "</pre>"
 echo "</body></html>"
--- ansible/roles/api-simulation/files/nginx/cgi-bin/switch.sh
+++ ansible/roles/api-simulation/files/nginx/cgi-bin/switch.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "Content-type: text/html"
 echo ""
-echo "<html><head><meta http-equiv='refresh' content='3;url=/useapi.php'></head>"
+echo "<html><head><meta http-equiv='refresh' content='5;url=/useapi.php'></head>"
 echo "<body style='font-family:sans-serif; padding:20px;'>"
 echo "<center>"
-echo "<h1>Switching to RHEL 10 on Petclinic Application Server</h1><hr><pre></center>"
-sudo -u lab-user ansible-playbook -v /home/lab-user/ansible/switch.yaml -i appserver, 2>&1
+echo "<h1>Switching to RHEL 10 on Petclinic Application Server and rebooting</h1><hr><pre></center>"
+sudo -i -u lab-user tmux new-session -d -s switch 'ansible-playbook -vvv /home/lab-user/ansible/switch.yaml -i appserver, 2>&1 > /tmp/switch_output.txt'
 echo "</pre>"
 echo "</body></html>"
